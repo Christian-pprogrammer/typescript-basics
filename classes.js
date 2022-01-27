5a50770e7f21284caf49b8497384dc7694708849
MPANO Christian <mpanoc6@gmail.com>
2022-01-27 14:44:23 +0200

    added typescript classes

---
+++ classes.js
@@ -0,0 +1,11 @@
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        //draw logic
+    };
+    Point.prototype.getDistance = function (another) {
+        //get distance login
+    };
+    return Point;
+}());
--- classes.ts
+++ classes.ts
@@ -3,10 +3,14 @@ class Point {
     y: number;
     draw() {
         //draw logic
+        console.log('drawing')
     }
 
     getDistance(another: Point) {
         //get distance login
+        console.log('get distance');
     }
 }
 
+let point: Point;
+point.draw();
